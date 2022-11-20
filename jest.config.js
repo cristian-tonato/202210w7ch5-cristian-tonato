916486362711e2e07677232ea81f7967300b74e0
Cristian <cristianotona@gmail.com>
2022-11-20 11:44:51 +0100

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+const preset = 'ts-jest';
+const testEnvironment = 'node';
+const testPathIgnorePatterns = ['dist'];
+const resolver = 'jest-ts-webcompat-resolver';
+
+export default { preset, testEnvironment, testPathIgnorePatterns, resolver };
